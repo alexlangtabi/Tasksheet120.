063d3bec490ce1cd4035ac57ae70d1742f5f39a0
alexlangtabi <68006914+alexlangtabi@users.noreply.github.com>
2024-10-22 01:25:21 +0800

    cumulative sum

--- cumulativeNumber/cumulative.java
+++ cumulativeNumber/cumulative.java
@@ -1,38 +1,33 @@
-
-package cumulativeNumber;
+package CumulativeNumber;
 
 public class cumulative {
-     int par4() {
+    public static int sum(int... numbers) {
         int sum = 0;
-        for (int i = 1; i <= 4; i++) {
-            sum += i;
+        for(int number:numbers){
+            sum+=number;
         }
         return sum;
-    }
 
-    int par5() {
-        int sum = 0;
-        for (int i = 1; i <= 5; i++) {
-            sum += i;
-        }
-        return sum;
     }
+    public static void main(String[] args) {
+        int[] nArray={1,2,3,4};
+        int comp=sum(nArray);
+        System.out.println(" Cumulative Sum for 4 : " +comp);
+        int[] nArray1={1,2,3,4,5};
+        int comp1=sum(nArray1);
+        System.out.println(" Cumulative Sum for 5 : " +comp1);
+        int[] nArray2={1,2,3,4,5,6,7,8,9,10};
+        int comp2=sum(nArray2);
+        System.out.println(" Cumulative Sum for 10: " +comp2);
+        System.out.println("Total sum of cumulative :"+(comp+comp1+comp2));
 
-    int par10() {
-        int sum = 0;
-        for (int i = 1; i <= 10; i++) {
-            sum += i;
-        }
-        return sum;
-    }
-   public static void main(String[] args) {
-        cumulative cum = new cumulative();
-        System.out.println("Cumulative sum for 4 is " + cum.par4()) ;
-        System.out.println("Cumulative sum for 5 is " + cum.par5()) ;
-        System.out.println("Cumulative sum for 10 is "  + cum.par10()) ;
-        System.out.println("Total sum of Cumulative : " + (cum.par4()+cum.par5()+cum.par10()));
+        //int comp= sum(nArray);
+
+
     }
 }
+
+
 
 
 
