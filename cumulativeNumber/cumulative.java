a4dfb3601113c0f60601da85f62884f3e870fab1
alexlangtabi <68006914+alexlangtabi@users.noreply.github.com>
2024-10-22 12:31:16 +0800

    cumulative.java

--- cumulativeNumber/cumulative.java
+++ cumulativeNumber/cumulative.java
@@ -12,17 +12,20 @@ public class cumulative {
         int[] par = {4,5,10};
        int total=0;
        int sFinal=0;
+       int gTotal=0;
         for (int i = 0; i <=2; i++) {
-            System.out.print(par[i]+ " = ");
+            System.out.print("\n"+par[i]+ " = ");
             for (int j = 1; j <= par[i]; j++) {
                 System.out.print(j+" ");
                 int [] sArray = {total +=j};
-                 sFinal=sum(sArray);
+                sFinal=sum(sArray);
             }
             System.out.println();
-            System.out.println("Cumulative Sum for "+ par[i]+" is " + sFinal);
+            System.out.println("\nCumulative Sum for "+ par[i]+" is " + sFinal);
             total-=total;
+            gTotal+=sFinal;
         }
+        System.out.println("\nTotal of Cumulative is : "+gTotal);
      }
 }
 
